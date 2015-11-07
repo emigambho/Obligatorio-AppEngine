ca755953024d37ef217bdb9f09a2b930f5bfc5d3
emigambho <emigambho@gmail.com>
2015-11-07 11:31:10 -0300

    test (cherry picked from commit ca0df69)

--- src/com/mkyong/controller/CustomerController.java
+++ src/com/mkyong/controller/CustomerController.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
-@RequestMapping("/customer fedeee")
+@RequestMapping("/customer")
 public class CustomerController {
 
 	//cambio en customer controller
